fd1c6967d7038a8f6ad799d439cf34b2764b71e3
zlodiak <prozaik81-2@yandex.ru>
2018-03-09 19:38:57 +0300

    form ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +16,8 @@ import { FormTemplateComponent } from './components/form-template/form-template.
     FormTemplateComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/components/form-reactive/form-reactive.component.ts
+++ src/app/components/form-reactive/form-reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form-reactive',
@@ -7,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormReactiveComponent implements OnInit {
 
+  currPass: string = '123456';
+  questions: string[] = [
+    'первый вопрос',
+    'второй вопрос',
+    'третий вопрос'
+  ];
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'currPass':         new FormControl(null, [ Validators.required, 
+                                                  Validators.minLength(6), 
+                                                  Validators.maxLength(10)], this.checkCurrPass.bind(this)),
+      'newPass':          new FormControl(null, [ Validators.required, 
+                                                  Validators.minLength(6), 
+                                                  Validators.maxLength(10)], this.checkPattern.bind(this)),
+      'newPassConfirm':   new FormControl(null, [ Validators.required,
+                                                  Validators.minLength(6), 
+                                                  Validators.maxLength(10)], [this.checkPattern.bind(this), this.checkconfirm.bind(this)]),
+      'newLogin':         new FormControl(null, [ Validators.required, 
+                                                  Validators.minLength(6), 
+                                                  Validators.maxLength(10)]),
+      'question':         new FormControl(null, Validators.required),
+      'answer':           new FormControl(null, [ Validators.required, 
+                                                  Validators.minLength(1), 
+                                                  Validators.maxLength(30)])
+    });    
+  }
+
+  checkCurrPass(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === this.currPass) {
+          resolve(null);
+        } else {
+          resolve({checkCurrPass: true});
+        } 
+      });     
+    });
+  }
+
+  checkPattern(control: FormControl): Promise<any>  {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = /[^A-ZА-Яa-zа-я\d!@#%&*;‘:“,./?.]/.test(control.value);
+        if(!result) {
+          resolve(null);
+        } else {
+          resolve({checkPatternNewPass: true});
+        } 
+      });     
+    });
+  }
+
+  checkconfirm(control: FormControl): Promise<any>  {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === this.form.value.newPass) {
+          resolve(null);
+        } else {
+          resolve({checkPatternNewPass: true});
+        } 
+      });     
+    });
+  }  
+
+  onSubmit(): void {
+    console.log('submit', this.form);
+  }
+
+  look() {
+    console.log(this.form);
   }
 
 }
